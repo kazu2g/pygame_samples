a849e2bb701ee95b21d8b3594dd3d6c4637e60d2
kazu2g <kazuhiro2g@gmail.com>
2024-02-19 22:35:12 +0900

    up

--- demo_04.py
+++ demo_04.py
@@ -1,36 +1,30 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
+from datetime import datetime
+import pygame
+from lcd_font_mc import LCD_font_mc
 
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat('kadai #1  the first golden block')
 
 mc.setBlock(0, 4, 0,  param.GOLD_BLOCK)
 
-# from datetime import datetime
-# import pygame
-# from lcd_font_pg import LCD_font
-# from seven_seg_pg import Seven_seg
+DARK_GRAY = (40, 40, 40)
+GRAY = (80, 80, 80)
+RED = (255, 0, 0)
+GREEN = (10, 250, 10)
+CYAN = (120, 120, 250)
+YELLOW = (250, 250, 20)
+WHITE = (250, 250, 250)
 
+pygame.init()
 
-# DARK_GRAY = (40, 40, 40)
-# GRAY = (80, 80, 80)
-# RED = (255, 0, 0)
-# GREEN = (10, 250, 10)
-# CYAN = (120, 120, 250)
-# YELLOW = (250, 250, 20)
-# WHITE = (250, 250, 250)
+clock = pygame.time.Clock()
 
-# pygame.init()
-
-# clock = pygame.time.Clock()
-# screen = pygame.display.set_mode([700, 320])
-# pygame.display.set_caption("pygame 7-segment display simulation")
-# screen.fill(DARK_GRAY)
-
-# # display1 = LCD_font(screen)
-# # display1.init_col(
-# #     BLOCK_SIZE=9, BLOCK_INTV=10, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
-# # display1.init_row(X_ORG=8, Y_ORG=22, COL_INTV=6)
+display1 = LCD_font_mc(mc)
+display1.init_col(
+    BLOCK_SIZE=4, BLOCK_INTV=2, COLOR_ON=param.GOLD_BLOCK, COLOR_OFF=param.AIR)
+display1.init_row(X_ORG=8, Y_ORG=22, COL_INTV=6)
 
 # # display2 = LCD_font(screen)
 # # display2.init_col(
@@ -44,32 +38,30 @@ mc.setBlock(0, 4, 0,  param.GOLD_BLOCK)
 # # display5.init_row(X_ORG=8, Y_ORG=8, COL_INTV=6)
 
 
-# running = True
-# # infinite loop top ----
-# while running:
-#     for count in range(16 ** 4):  # 0から65535まで
-#         # press ctrl-c or close the window to stop
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 running = False
-#         if not running:
-#             break
+running = True
+# infinite loop top ----
+while running:
+    for count in range(16 ** 4):  # 0から65535まで
+        # press ctrl-c or close the window to stop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        if not running:
+            break
 #         # 「for count」のループから抜ける。whileループも抜ける。
-#         dt_now = datetime.now()
-#         time_now = (dt_now.hour * 10000
-#                     + dt_now.minute * 100
-#                     + dt_now.second)
+        dt_now = datetime.now()
+        time_now = (dt_now.hour * 10000
+                     + dt_now.minute * 100
+                     + dt_now.second)
 #         display5.disp_num2(zfil=True, rjust=6, num=time_now, base=10)
-#         display1.update_col(col=0, code=(dt_now.hour/10)% 6)
-#         display1.update_col(col=1, code=(dt_now.hour%10))
-#         display1.update_col(col=2, code=10)
-#         display1.update_col(col=3, code=(dt_now.minute/10)% 6)   # 4096の位
-#         display1.update_col(col=4, code=dt_now.minute%10)   # 256の位
-#         display1.update_col(col=5, code=10)
-#         display1.update_col(col=6, code=(dt_now.second/10)% 6)          # 16の位
-#         display1.update_col(col=7, code=dt_now.second % 10)             # 1の位
-
-
+        display1.update_col(col=7, code=(dt_now.hour/10)% 6)
+        display1.update_col(col=6, code=(dt_now.hour%10))
+        display1.update_col(col=5, code=10)
+        display1.update_col(col=4, code=(dt_now.minute/10)% 6)   # 4096の位
+        display1.update_col(col=3, code=dt_now.minute%10)   # 256の位
+        display1.update_col(col=2, code=10)
+        display1.update_col(col=1, code=(dt_now.second/10)% 6)          # 16の位
+        display1.update_col(col=0, code=dt_now.second % 10)             # 1の位
 
 #         display2.update_col(col=0, code=dt_now.year/1000)
 #         display2.update_col(col=1, code=dt_now.year%1)
@@ -82,9 +74,6 @@ mc.setBlock(0, 4, 0,  param.GOLD_BLOCK)
 #         display2.update_col(col=8, code=dt_now.day/10)
 #         display2.update_col(col=9, code=dt_now.day%10)
 
-#         pygame.display.flip()  # update_col
-#         clock.tick(20)  # FPS, Frame Per Second
-#     screen.fill(DARK_GRAY)
-# infinit loop bottom ----
+
 
 pygame.quit()
---
+++ lcd_font_mc.py
@@ -0,0 +1,166 @@
+# handmade LCD font for pygame
+# 5x7ドットマトリクス
+
+# from math import log
+import pygame
+from pygame.locals import Rect
+from mcje.minecraft import Minecraft
+import param_MCJE as param
+mc = Minecraft.create(port=param.PORT_MC)
+
+
+LCD_0 = (0, 1, 1, 1, 0,
+         1, 0, 0, 0, 1,
+         1, 0, 0, 1, 1,
+         1, 0, 1, 0, 1,
+         1, 1, 0, 0, 1,
+         1, 0, 0, 0, 1,
+         0, 1, 1, 1, 0)
+
+LCD_1 = (0, 0, 1, 0, 0,
+         0, 1, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 0, 1, 0, 0,
+         0, 1, 1, 1, 0)
+
+LCD_2 = (0, 1, 1, 1, 0,
+         1, 0, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0,
+         0, 1, 0, 0, 0,
+         1, 1, 1, 1, 1)
+
+LCD_3 = (0, 1, 1, 1, 0,
+         1, 0, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 1, 1, 1,
+         0, 0, 0, 0, 1,
+         1, 0, 0, 0, 1,
+         0, 1, 1, 1, 0)
+
+LCD_4 = (0, 0, 0, 1, 0,
+         0, 0, 1, 1, 0,
+         0, 1, 0, 1, 0,
+         1, 0, 0, 1, 0,
+         1, 1, 1, 1, 1,
+         0, 0, 0, 1, 0,
+         0, 0, 0, 1, 0)
+
+LCD_5 = (1, 1, 1, 1, 1,
+         1, 0, 0, 0, 0,
+         1, 1, 1, 1, 0,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         1, 0, 0, 0, 1,
+         0, 1, 1, 1, 0)
+
+LCD_6 = (0, 1, 1, 1, 0,
+         1, 0, 0, 0, 0,
+         1, 0, 0, 0, 0,
+         0, 1, 1, 1, 0,
+         1, 0, 0, 0, 1,
+         1, 0, 0, 0, 1,
+         0, 1, 1, 1, 0)
+
+LCD_7 = (1, 1, 1, 1, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 1, 0,
+         0, 0, 1, 0, 0,
+         0, 1, 0, 0, 0,
+         0, 1, 0, 0, 0,
+         0, 1, 0, 0, 0)
+
+LCD_8 = (0, 1, 1, 1, 0,
+         1, 0, 0, 0, 1,
+         1, 0, 0, 0, 1,
+         0, 1, 1, 1, 0,
+         1, 0, 0, 0, 1,
+         1, 0, 0, 0, 1,
+         0, 1, 1, 1, 0)
+
+LCD_9 = (0, 1, 1, 1, 0,
+         1, 0, 0, 0, 1,
+         1, 0, 0, 0, 1,
+         0, 1, 1, 1, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 1, 0,
+         0, 1, 1, 0, 0)
+
+LCD_10 = (0, 0, 0, 0, 0,
+          0, 1, 1, 0, 0,
+          0, 1, 1, 0, 0,
+          0, 0, 0, 0, 0,
+          0, 1, 1, 0, 0,
+          0, 1, 1, 0, 0,
+          0, 0, 0, 0, 0)
+
+LCD_11 = (0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0,
+          1, 1, 1, 1, 1,
+          0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0)
+
+LCD_font_styles = (LCD_0, LCD_1, LCD_2, LCD_3, LCD_4, LCD_5,
+                   LCD_6, LCD_7, LCD_8, LCD_9, LCD_10, LCD_11)
+
+DARK_GRAY = (40, 40, 40)
+GRAY = (80, 80, 80)
+RED = (255, 0, 0)
+GREEN = (10, 250, 10)
+YELLOW = (250, 250, 20)
+WHITE = (250, 250, 250)
+
+
+class LCD_font_mc():
+    def __init__(self, mc):
+        self.screen = mc
+    # def __init__(self, screen):
+    #     self.screen = screen
+
+    def init_col(self, BLOCK_SIZE=2,
+                 BLOCK_INTV=2, COLOR_ON=WHITE, COLOR_OFF=GRAY):
+        # ひと桁、コラムの設定
+        # ブロックのサイズと配置間隔をピクセル指定（インターバル）
+        self.BLOCK_SIZE = BLOCK_SIZE
+        self.BLOCK_INTV = BLOCK_INTV
+        # on/offのカラー
+        self.COLOR_ON = COLOR_ON
+        self.COLOR_OFF = COLOR_OFF
+
+    def init_row(self, X_ORG=2, Y_ORG=8, COL_INTV=6):  # 表示行の設定
+        # xy空間での7セグ表示、最上位桁の左下座標をブロック数で指定
+        self.X_ORG = X_ORG * self.BLOCK_INTV
+        self.Y_ORG = Y_ORG * self.BLOCK_INTV
+        # 各桁のブロック間隔をブロック数で指定（インターバル）
+        self.COL_INTV = COL_INTV * self.BLOCK_INTV
+
+    def update_col(self, col=0, code=2):  # ある桁にある文字を表示する関数
+        # codeの文字をcol桁目に表示、桁は最上位桁の左から右へ進む。
+        # block_size = self.BLOCK_SIZE
+        i = 0
+        for y in range(7):
+            for x in range(5):
+                x0 = self.X_ORG + self.COL_INTV * col
+                y0 = self.Y_ORG
+                # ドットの原点座標
+                org1 = (x0 - x * self.BLOCK_INTV)
+                org2 = (y0 - y * self.BLOCK_INTV)
+                if LCD_font_styles[int(code)][i] == 1:
+                    mc.setBlock(org1, org2, 0,  param.GOLD_BLOCK)
+
+                else:
+                    mc.setBlock(org1, org2, 0,  param.AIR)
+                # 桁の原点
+
+                # ドットを描く
+
+
+                #  pygame.draw.rect(self.screen, color,
+                #                  Rect(org1,
+                #                       org1[1], block_size, block_size))
+                i += 1
